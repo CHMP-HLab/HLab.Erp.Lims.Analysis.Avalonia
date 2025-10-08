7805b3bbf9db059214d92eefc0d8e98c99312782
mathieu <m@thieu-gre.net>
2025-10-08 15:28:21 +0200

    fusion

--- HLab.Erp.Lims.Analysis.Avalonia/HLab.Erp.Lims/HLab.Erp.Lims.Analysis.Wpf/FormClasses/FormClassProvider.cs
+++ HLab.Erp.Lims.Analysis.Avalonia/HLab.Erp.Lims/HLab.Erp.Lims.Analysis.Wpf/FormClasses/FormClassProvider.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Markup;
+using AvalonDock.Controls;
 using HLab.Base;
 using HLab.Base.Wpf.Extensions;
 using HLab.Compiler;
